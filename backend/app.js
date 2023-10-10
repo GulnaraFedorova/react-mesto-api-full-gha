25fcc23641c4e9d44ecf73a95e5e197f3a4d70b8
Gulnara Fedorova <gul-khurmatova@yandex.ru>
2023-10-10 20:35:43 +0200

    15й спринт 2я итерация

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,8 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const cors = require('cors');
 
-const { PORT = 4000, DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
-//const { PORT = 4000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
+// const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
